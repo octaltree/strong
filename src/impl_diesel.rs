0157b5e5c39a95857eba37025f49dcc00656051d
octaltree <octaltree@gmail.com>
2021-02-23 18:29:20 +0900

    asexpression

--- src/impl_diesel.rs
+++ src/impl_diesel.rs
@@ -1,10 +1,14 @@
-use crate::{marker, StrongBuf, Validator};
+use crate::{marker, Strong, StrongBuf, Validator};
 use diesel::pg::Pg;
 use std::marker::PhantomData;
 
 #[derive(SqlType, QueryId)]
 #[postgres(oid = "25", array_oid = "1009")]
 pub struct SqlStrong<Ctx>(PhantomData<Ctx>);
+
+impl<Ctx> Default for SqlStrong<Ctx> {
+    fn default() -> Self { SqlStrong(PhantomData) }
+}
 
 impl<Ctx> diesel::serialize::ToSql<SqlStrong<Ctx>, Pg> for StrongBuf<Ctx>
 where
@@ -30,4 +34,48 @@ where
     }
 }
 
-// TODO: AsExpression
+fn _impl_as_expression() {
+    use diesel::{
+        expression::{bound::Bound, AsExpression},
+        serialize::{self, Output, ToSql},
+        sql_types::Nullable
+    };
+    impl<'expr, Ctx> AsExpression<SqlStrong<Ctx>> for &'expr Strong<Ctx>
+    where
+        Ctx: Validator
+    {
+        type Expression = Bound<SqlStrong<Ctx>, Self>;
+        fn as_expression(self) -> Self::Expression { Bound::new(self) }
+    }
+    impl<'expr, Ctx> AsExpression<Nullable<SqlStrong<Ctx>>> for &'expr Strong<Ctx>
+    where
+        Ctx: Validator
+    {
+        type Expression = Bound<Nullable<SqlStrong<Ctx>>, Self>;
+        fn as_expression(self) -> Self::Expression { Bound::new(self) }
+    }
+    impl<__DB, Ctx> diesel::serialize::ToSql<Nullable<SqlStrong<Ctx>>, __DB> for Strong<Ctx>
+    where
+        Ctx: Validator,
+        __DB: diesel::backend::Backend,
+        Self: ToSql<SqlStrong<Ctx>, __DB>
+    {
+        fn to_sql<W: std::io::Write>(&self, out: &mut Output<W, __DB>) -> serialize::Result {
+            ToSql::<SqlStrong<Ctx>, __DB>::to_sql(self, out)
+        }
+    }
+    impl<Ctx> AsExpression<SqlStrong<Ctx>> for StrongBuf<Ctx>
+    where
+        Ctx: Validator
+    {
+        type Expression = Bound<SqlStrong<Ctx>, Self>;
+        fn as_expression(self) -> Self::Expression { Bound::new(self) }
+    }
+    impl<Ctx> AsExpression<Nullable<SqlStrong<Ctx>>> for StrongBuf<Ctx>
+    where
+        Ctx: Validator
+    {
+        type Expression = Bound<Nullable<SqlStrong<Ctx>>, Self>;
+        fn as_expression(self) -> Self::Expression { Bound::new(self) }
+    }
+}
